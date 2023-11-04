7cf9928e47ce58d06b5ab37835b0695ebd08fbf8
Brandon Carrillo Álvarez <128259306+BrandonCA859@users.noreply.github.com>
2023-11-03 23:15:49 -0600

    Cambios de company

--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -1,26 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCompanyDto } from './dto/create-company.dto';
 import { UpdateCompanyDto } from './dto/update-company.dto';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { Company } from './entities/company.entity';
+import { Repository } from 'typeorm';
 @Injectable()
 export class CompaniesService {
+
+  constructor(
+    @InjectRepository(Company)
+    private companyRepository: Repository<Company>
+  ){}
   create(createCompanyDto: CreateCompanyDto) {
-    return 'This action adds a new company';
+    const addedCompany = this.companyRepository.create(createCompanyDto);
+     this.companyRepository.save(addedCompany);
+     return addedCompany;
   }
 
   findAll() {
-    return `This action returns all companies`;
+    return this.companyRepository.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} company`;
+    this.companyRepository.findOneBy({id});
   }
 
-  update(id: number, updateCompanyDto: UpdateCompanyDto) {
-    return `This action updates a #${id} company`;
+  async update(id: number, updateCompanyDto: UpdateCompanyDto) {
+    const updatedCompany = await this.companyRepository.update({ id }, updateCompanyDto);
+    return updatedCompany;
   }
 
   remove(id: number) {
-    return `This action removes a #${id} company`;
+    return this.companyRepository.delete(id);
   }
 }
+
