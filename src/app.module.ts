815185ea8ca7dce1dbfe5afba28619eb54b78636
José Pablo Suárez Gómez <suarezgomezjosepablo03@gmail.com>
2023-11-11 17:56:44 -0600

    Fix password field in app.module.ts and update Company entity.

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,7 +18,7 @@ import { Service } from './services/entities/service.entity';
         host: 'localhost',
         port: 3306,
         username: 'root',
-        password: 'Aaron2003#%',
+        password: '',
         database: 'company',
         entities: [Company,Galleries,Event,Service],
         autoLoadEntities: true,
