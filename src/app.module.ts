f35b015661e8f0ae632fe20a8491f4381c18b4c3
Hezron <hezroncr@gmail.com>
2023-11-15 23:02:19 -0600

    Merge pull request #7 from JosePabloSG/Hezron Change Service

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,7 +18,7 @@ import { Service } from './services/entities/service.entity';
         host: 'localhost',
         port: 3306,
         username: 'root',
-        password: '2710',
+        password: '',
         database: 'company',
         entities: [Company,Galleries,Event,Service],
         autoLoadEntities: true,
--- src/services/services.service.ts
+++ src/services/services.service.ts
@@ -3,6 +3,7 @@ import { UpdateServiceDto } from './dto/update-service.dto';
 import { Service } from './entities/service.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateServiceDto } from './dto/create-service.dto';
 
 @Injectable()
 export class ServicesService {
@@ -11,18 +12,23 @@ export class ServicesService {
     @InjectRepository(Service)
     private serviceRepository: Repository<Service>){}
 
+  create(createServiceDto: CreateServiceDto) {
+    const addedServices = this.serviceRepository.create(createServiceDto);
+    this.serviceRepository.save(addedServices);
+    return addedServices;
+  }
+
   findAll() {
     return this.serviceRepository.find();
   }
 
   findOne(id: number) {
-    return this.serviceRepository.findOneBy({id});
+    return this.serviceRepository.findOneBy({id})
   }
 
-  async update(id: number, updateServiceDto: UpdateServiceDto) {
-    
-    const updatedServiceDto = await this.serviceRepository.update({id},updateServiceDto);
-    return updatedServiceDto;
+  async update(id: number, updateServiceDto: UpdateServiceDto)  {
+    const updatedService = await this.serviceRepository.update({id},updateServiceDto);
+    return updatedService;
   }
   
 
